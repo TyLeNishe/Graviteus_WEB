6f0fb9525fdfb0c84d8fec390c9fd2c4627b07cf
Bridal <iliafedoseenko2008@gmail.com>
2025-03-01 13:26:12 +0300

    First

--- graviteus_website/graviapps/views.py
+++ graviteus_website/graviapps/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm, LoginForm
 
 def home(request):
     return render(request, 'home.html')
+def SignUp(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'SignUp.html', {'form': form})
+
+def LogIn(request):
+    form = LoginForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    return render(request, 'LogIn.html', {'form': form})
