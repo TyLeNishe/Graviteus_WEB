b5aed7951f0f0492c3b79e704ed2c7e97c9b010d
PricoldeZ <gimnazija006@gmail.com>
2025-04-19 03:09:23 +0200

    Небольшой патч. справлена карусель изображений (раньше стрелочки были без стилей), небольшие изменения в падающих символах и коде профиля, добавлен новый шрифт

--- graviteus_website/graviteus_website/urls.py
+++ graviteus_website/graviteus_website/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('graviapps.urls')),
-    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
