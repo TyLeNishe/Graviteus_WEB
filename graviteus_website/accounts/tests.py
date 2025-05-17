8dd93dbff287136dabf69f6859aa25a78defce2a
gogolplay <106255631+gogolplay@users.noreply.github.com>
2025-05-17 11:28:40 +0300

    Тесты

--- graviteus_website/accounts/tests.py
+++ graviteus_website/accounts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class AccountsTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertTrue(self.user.check_password('12345'))
+
+    def test_login_valid(self):
+        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': '12345'})
+        self.assertEqual(response.status_code, 302)  # редирект при успешном входе
+
+    def test_login_invalid(self):
+        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'wrong'})
+        self.assertNotEqual(response.status_code, 302)
+
+    def test_logout(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_user(self):
+        self.client.login(username='testuser', password='12345')
+        self.user.delete()
+        self.assertFalse(User.objects.filter(username='testuser').exists())
